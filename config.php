88668af4b5118daf967ae1a3b3c496503e522027
Timandes White <timands@gmail.com>
2017-01-12 18:54:27 +0800

    Import template of 'index'

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 配置文件
+ *
+ * @package timandes\crawler
+ */
+
+$GLOBALS['gaDBConfig'] = array(
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'user' => 'root',
+        'password' => '123456a',
+    );
--- public/index.php
+++ public/index.php
@@ -7,9 +7,23 @@
 
 require_once realpath(__DIR__ . '/../') . '/vendor/autoload.php';
 
-$foo = new Foo();
-$foo = new \enterprise\Foo();
+require_once realpath(__DIR__ . '/../') . '/config.php';
+
 $smarty = new Smarty();
 $smarty->setTemplateDir(realpath(__DIR__ . '/../') . '/templates/');
 $smarty->setCompileDir(realpath(__DIR__ . '/../') . '/templates_c/');
+
+$siteId = 1;
+$domain = 'snackmakingmachineoff.com';
+$translationDAO = new \crawler\daos\Translation();
+$rows = $translationDAO->getBySite($siteId);
+$translations = array();
+foreach ($rows as $row) {
+    $row['abbr'] = \crawler\daos\Translation::locale2Abbr($row['locale']);
+    $row['text'] = \crawler\daos\Translation::locale2Text($row['locale']);
+    $row['url'] = 'http://' . $row['locale'] . '.' . $domain . '/';
+    $translations[] = $row;
+}
+$smarty->assign('translations', $translations);
+
 $smarty->display('index.tpl');
