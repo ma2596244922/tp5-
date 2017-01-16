085ff1c094fdedf091a164b2b03e84813dc9f284
Timandes White <timands@gmail.com>
2017-01-16 10:24:50 +0800

    Display whole object(page, image, etc.) when requested

--- public/index.php
+++ public/index.php
@@ -6,8 +6,53 @@
  */
 
 require_once realpath(__DIR__ . '/../') . '/vendor/autoload.php';
-
 require_once realpath(__DIR__ . '/../') . '/config.php';
+
+// TODO:
+$siteId = 1;
+$originalDomainSuffix = 'snackmakingmachine.com';
+$currentDomainSuffix = 'snackmakingmachineoff.com';
+
+// 优先使用本地存储的页面、图片和资源等
+$requestRelativeURI = $_SERVER['REQUEST_URI'];
+$fakeRequestHost = str_replace($currentDomainSuffix, $originalDomainSuffix, $_SERVER['HTTP_HOST']);
+$requestURL = 'http://' . $fakeRequestHost . $requestRelativeURI;
+$requestPath = parse_url($requestURL, PHP_URL_PATH);
+$urlSum = md5($requestURL, true);
+$pathSum = md5($requestPath, true);
+// + Page
+$pageDAO = new \crawler\daos\Page();
+$page = $pageDAO->getByUrlSum($urlSum);
+if ($page) {
+    if ($page['status_code'] != 200) {
+        http_response_code($page['status_code']);
+        exit(1);
+    }
+
+    echo str_replace($originalDomainSuffix, $currentDomainSuffix, $page['content']);
+    exit(0);
+}
+// + Image N Resource
+$daos = array(
+        new \crawler\daos\Image(),
+        new \crawler\daos\Resource(),
+    );
+foreach ($daos as $dao) {
+    $image = $dao->getByMutex($siteId, $pathSum);
+    if ($image) {
+        if ($image['status_code'] != 200) {
+            http_response_code($image['status_code']);
+            exit(1);
+        }
+
+        header('Content-Type: ' . $image['content_type']);
+        echo $image['content'];
+        exit(0);
+    }
+}
+
+http_response_code(404);
+exit(1);
 
 $smarty = new Smarty();
 $smarty->setTemplateDir(realpath(__DIR__ . '/../') . '/templates/');
