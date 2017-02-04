c51216023eae80ad475e02f8b04b8e98e9073198
Timandes White <timands@gmail.com>
2017-02-04 18:47:40 +0800

    Move functions

--- enterprise.h.php
+++ enterprise.h.php
@@ -5,6 +5,46 @@
  * @package timandes\enterprise
  */
 
+function enterprise_generate_guid()
+{
+    return md5(uniqid(mt_rand(), true), true);
+}
+
+/**
+ * 过滤参数值
+ *
+ * @param mixed $mValue 参数值
+ * @param string $sFilters 过滤函数清单
+ * @return mixed
+ */
+function enterprise_filter($mValue, $sFilters = 'trim, strip_tags')
+{
+    if (!is_string($mValue))
+        return $mValue; // skip non-string
+
+    // 使用过滤函数
+    if(!empty($sFilters)) {
+        $aFilters = explode(',', $sFilters);
+        if(is_array($aFilters)) foreach($aFilters as $f) {
+            $f = trim($f);
+            eval("\$mValue = {$f}(\$mValue);");
+        }
+    }
+
+    return $mValue;
+}
+
+function enterprise_get_post_data($key, $filters = 'trim, strip_tags')
+{
+    $v = (isset($_POST[$key])?$_POST[$key]:'');
+    return enterprise_filter($v, $filters);
+}
+
+function enterprise_get_query_data($key, $filters = 'trim, strip_tags')
+{
+    $v = (isset($_GET[$key])?$_GET[$key]:'');
+    return enterprise_filter($v, $filters);
+}
 
 /**
  * 展示Sitemap Index页面
--- public/index.php
+++ public/index.php
@@ -12,40 +12,6 @@ require_once realpath(__DIR__ . '/../') . '/vendor/autoload.php';
 require_once realpath(__DIR__ . '/../') . '/config.php';
 require_once realpath(__DIR__ . '/../') . '/enterprise.h.php';
 
-function enterprise_generate_guid()
-{
-    return md5(uniqid(mt_rand(), true), true);
-}
-
-/**
- * 过滤参数值
- *
- * @param mixed $mValue 参数值
- * @param string $sFilters 过滤函数清单
- * @return mixed
- */
-function enterprise_filter($mValue, $sFilters = 'trim, strip_tags')
-{
-    if (!is_string($mValue))
-        return $mValue; // skip non-string
-
-    // 使用过滤函数
-    if(!empty($sFilters)) {
-        $aFilters = explode(',', $sFilters);
-        if(is_array($aFilters)) foreach($aFilters as $f) {
-            $f = trim($f);
-            eval("\$mValue = {$f}(\$mValue);");
-        }
-    }
-
-    return $mValue;
-}
-
-function enterprise_get_post_data($key, $filters = 'trim, strip_tags')
-{
-    $v = (isset($_POST[$key])?$_POST[$key]:'');
-    return enterprise_filter($v, $filters);
-}
 
 function enterprise_print_cnzz($meta)
 {
