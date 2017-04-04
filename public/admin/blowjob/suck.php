9e6b001b7773c1cb2fb3b4b14ac2ec064c1329d2
Timandes White <timands@gmail.com>
2017-04-04 12:55:47 +0800

    Response groups

--- public/admin/blowjob/suck.php
+++ public/admin/blowjob/suck.php
@@ -9,6 +9,7 @@ require_once __DIR__ . '/bootstrap.php';
 
 list($siteId, $locale, $originalDomainSuffix, $currentDomainSuffix) = enterprise_extract_site_infos();
 
+// Get tasks
 $taskDAO = new blowjob\daos\Task();
 $groupDAO = new enterprise\daos\Group();
 $statusRange = blowjob\daos\Task::STATUS_PENDING . ', ' . blowjob\daos\Task::STATUS_IN_PROGRESS;
@@ -20,6 +21,10 @@ $sql = "SELECT t.`id`, t.`group_id`, g.`name` AS `group_name`, t.`target_url`, t
     LEFT JOIN `{$groupTableName}` g ON g.`id`=t.`group_id`
     WHERE {$condition} ORDER BY t.`id` DESC";
 $tasks = $taskDAO->getMultiBySql($sql);
+
+// Get groups
+$condition = '`site_id`=' . (int)$siteId . ' AND `deleted`=0';
+$groups = $groupDAO->getMultiInOrderBy($condition, '`id`, `name`', '`id` ASC');
 
 $response = array(
         "host"=> "www." . $currentDomainSuffix,
@@ -33,6 +38,7 @@ $response = array(
             "url"=> "/admin/blowjob/sexmeup.php"
         ),
         'tasks' => $tasks,
+        'groups' => $groups,
     );
 
 header('Content-type: application/json');
