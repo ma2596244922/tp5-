5bd451ed53ef5c3acd1eb73431f6c46d2d29333a
Timandes White <timands@gmail.com>
2017-05-24 18:01:04 +0800

    Add class LangSite.

---
+++ src/daos/LangSite.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Enterprise
+ *
+ * @package timandes\enterprise
+ */
+
+namespace enterprise\daos;
+
+/**
+ * LangSite DAO
+ */
+class LangSite extends \crawler\daos\AbstractDAO
+{
+    protected $_fields = array(
+            'site_id' => 'int',
+            'updated' => 'text',
+            'index_html_title' => 'text',
+            'index_meta_keywords' => 'text',
+            'index_meta_description' => 'text',
+            'desc_4_inquiry_sender' => 'text',
+            'product_tdk_scope' => 'tinyint',
+            'product_html_title' => 'text',
+            'product_meta_keywords' => 'text',
+            'product_meta_description' => 'text',
+            'contact_content' => 'text',
+        );
+
+    private $_langCode = '';
+
+    public function __construct($langCode)
+    {
+        $supportedLangCodes = \enterprise\LangCode::getSupportedLangCodes();
+        if (!isset($supportedLangCodes[$langCode]))
+            throw new \DomainException("Unsupported lang {$langCode}");
+
+        $this->_langCode = $langCode;
+    }
+
+    public function getTableName()
+    {
+        return 'enterprise_' . $this->_langCode . '_sites';
+    }
+
+    public function getDbName()
+    {
+        return 'crawler';
+    }
+    
+    public function insert($values)
+    {
+        $tableName = $this->getTableName();
+        $dbName = $this->getDbName();
+
+        $db = \DbFactory::create($dbName);
+
+        $sets = $this->buildSetList($db, $values);
+        $setsString = implode(',', $sets);
+        $sql = "INSERT INTO `{$tableName}` SET {$setsString}";
+        $r = $db->query($sql);
+        if (!$r)
+            throw new \RuntimeException("Fail to query sql(#{$db->errno}, {$db->error}): {$sql}");
+    }
+
+    public function update($siteId, $values)
+    {
+        $tableName = $this->getTableName();
+        $dbName = $this->getDbName();
+
+        $db = \DbFactory::create($dbName);
+
+        $siteId = (int)$siteId;
+        $sets = $this->buildSetList($db, $values);
+        $setsString = implode(',', $sets);
+        $sql = "UPDATE `{$tableName}` SET {$setsString} WHERE `site_id`={$siteId}";
+        $r = $db->query($sql);
+        if (!$r)
+            throw new \RuntimeException("Fail to query: {$sql}");
+    }
+}
