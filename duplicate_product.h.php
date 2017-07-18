992197d34eb06be6b9f08893fddbf9bf465af987
Timandes White <timands@gmail.com>
2017-07-18 17:27:13 +0800

    Extract function 'duplicate_product_source_group'

---
+++ duplicate_product.h.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * 产品复制程序函数库
+ *
+ * @package timandes\enterprise
+ */
+
+
+/**
+ * 复制源分组
+ */
+function duplicate_product_source_group($sourceGroupId, $sourceLangCode, $targetSiteId, $targetGroupId, $targetLangCode, $fromProductId)
+{
+    $verbose = $GLOBALS['gaSettings']['verbose'];
+
+    if ($verbose >= 2)
+        fprintf(STDOUT, "Duplicating source group {$sourceGroupId} ..." . PHP_EOL);
+
+    // Get source group
+    $sourceGroup = enterprise_get_group_info($sourceGroupId, $sourceLangCode);
+    if (!$sourceGroup)
+        throw new \RuntimeException("Could not find source group {$sourceGroupId}");
+    if ($verbose >= 2)
+        fprintf(STDOUT, "Source Group: #{$sourceGroupId} {$sourceGroup['name']}" . PHP_EOL);
+    $sourceSiteId = $sourceGroup['site_id'];
+
+    if ($targetSiteId) {// Duplicate New Group
+        // Get target language site
+        if ($targetLangCode == 'en')
+            $siteDAO = new \enterprise\daos\Site();
+        else
+            $siteDAO = new \enterprise\daos\LangSite($targetLangCode);
+        $condition = "`site_id`=" . (int)$targetSiteId;
+        $site = $siteDAO->getOneBy($condition);
+        if (!$site)
+            throw new \RuntimeException("Could not find target {$targetLangCode} site {$targetSiteId}");
+        if ($verbose >= 2)
+            fprintf(STDOUT, "Target Site: #{$targetSiteId} {$targetLangCode}" . PHP_EOL);
+
+        // Duplicate Group
+        $targetGroupId = enterprise_admin_save_group($targetLangCode, 0, $sourceGroup['name'], $targetSiteId, $sourceGroup['path']);
+        if ($verbose >= 2)
+            fprintf(STDOUT, "Target group #{$targetGroupId} created" . PHP_EOL);
+    } else {// Use Existing Group
+        // Get target language group
+        $group = enterprise_get_group_info($targetGroupId, $targetLangCode);
+        if (!$group)
+            throw new \RuntimeException("Could not find target {$targetLangCode} group {$targetGroupId}");
+        if ($verbose >= 2)
+            fprintf(STDOUT, "Target Group: #{$targetGroupId} {$targetLangCode}" . PHP_EOL);
+    }
+
+    // Duplicate Products
+    if ($sourceLangCode == 'en')
+        $productDAO = new \enterprise\daos\Product();
+    else
+        $productDAO = new \enterprise\daos\LangProduct($sourceLangCode);
+    $curProductId = ($fromProductId>0?$fromProductId:0);
+    do {
+        if ($sourceLangCode == 'en')
+            $products = enterprise_get_product_list($sourceSiteId, $sourceLangCode, $sourceGroupId, 1, 100, "`id`>{$curProductId}", '`id` ASC', '`tags`, `description`, `specifications`, `images`, `site_id`');
+        else
+            $products = enterprise_get_product_list($sourceSiteId, $sourceLangCode, $sourceGroupId, 1, 100, "elp.`product_id`>{$curProductId}", 'elp.`product_id` ASC', 'elp.`tags`, elp.`description`, elp.`specifications`, ep.`images`, elp.`site_id`');
+        if (!$products)
+            break;
+
+        if ($verbose >= 2)
+            fprintf(STDOUT, "Found " . count($products) . " product(s)" . PHP_EOL);
+
+        foreach ($products as $product) {
+            if ($verbose >= 3)
+                fprintf(STDOUT, "Duplicating product {$product['id']} ..." . PHP_EOL);
+
+            $product['specifications'] = ($product['specifications']?json_decode($product['specifications'], true):[]);
+            $product['images'] = ($product['images']?json_decode($product['images'], true):[]);
+
+            enterprise_admin_save_product($targetLangCode, 0, $product['brand_name'], $product['model_number'], $product['certification'], $product['place_of_origin'], $product['price'], $product['payment_terms'], $product['supply_ability'], $product['head_image_id'], $product['images'], $targetSiteId, $product['caption'], $product['description'], $targetGroupId, $product['min_order_quantity'], $product['delivery_time'], $product['packaging_details'], $product['specifications'], $product['tags']);
+
+            $curProductId = $product['id'];
+        }
+    } while(true);
+
+    if ($verbose >= 2)
+        fprintf(STDOUT, "Finish duplicate procedure for source group {$sourceGroupId}" . PHP_EOL);
+}
--- duplicate_product.php
+++ duplicate_product.php
@@ -1,7 +1,7 @@
 #! /bin/env php
 <?php
 /**
- * 将某站英文分组下的产品复制到另外一个站的葡萄牙语分站下
+ * 将某站某分组下的产品复制到另外一个站的分站下
  *
  * @package timandes\enterprise
  */
@@ -24,6 +24,7 @@ $GLOBALS['gaSettings'] = array(
 
 require_once __DIR__ . '/bootstrap.php';
 require_once __DIR__ . '/config_admin.php';
+require_once __DIR__ . '/duplicate_product.h.php';
 
 function usage()
 {
@@ -54,84 +55,18 @@ function duplicate_proc()
 {
     $verbose = $GLOBALS['gaSettings']['verbose'];
     $sourceGroupIdList = $GLOBALS['gaSettings']['source_group_id_list'];
-
-    $sourceGroupIdArray = explode(',', $sourceGroupIdList);
-    if (is_array($sourceGroupIdArray)) foreach ($sourceGroupIdArray as $id) {
-        duplicate_source_group($id);
-    }
-
-    if ($verbose >= 2)
-        fprintf(STDOUT, "Finish duplicate procedure" . PHP_EOL);
-}
-
-function duplicate_source_group($sourceGroupId)
-{
-    $verbose = $GLOBALS['gaSettings']['verbose'];
     $sourceLangCode = $GLOBALS['gaSettings']['source_lang_code'];
     $targetSiteId = $GLOBALS['gaSettings']['target_site_id'];
     $targetLangCode = $GLOBALS['gaSettings']['target_lang_code'];
     $fromProductId = $GLOBALS['gaSettings']['from_product_id'];
 
-    if ($verbose >= 2)
-        fprintf(STDOUT, "Duplicating source group {$sourceGroupId} ..." . PHP_EOL);
-
-    // Get source group
-    $sourceGroup = enterprise_get_group_info($sourceGroupId, $sourceLangCode);
-    if (!$sourceGroup)
-        throw new \RuntimeException("Could not find source group {$sourceGroupId}");
-    if ($verbose >= 2)
-        fprintf(STDOUT, "Source Group: #{$sourceGroupId} {$sourceGroup['name']}" . PHP_EOL);
-    $sourceSiteId = $sourceGroup['site_id'];
-
-    // Get target language site
-    if ($targetLangCode == 'en')
-        $siteDAO = new \enterprise\daos\Site();
-    else
-        $siteDAO = new \enterprise\daos\LangSite($targetLangCode);
-    $condition = "`site_id`=" . (int)$targetSiteId;
-    $site = $siteDAO->getOneBy($condition);
-    if (!$site)
-        throw new \RuntimeException("Could not find target {$targetLangCode} site {$targetSiteId}");
-    if ($verbose >= 2)
-        fprintf(STDOUT, "Target Site: #{$targetSiteId} {$targetLangCode}" . PHP_EOL);
-
-    // Duplicate Group
-    $targetGroupId = enterprise_admin_save_group($targetLangCode, 0, $sourceGroup['name'], $targetSiteId, $sourceGroup['path']);
-    if ($verbose >= 2)
-        fprintf(STDOUT, "Target group #{$targetGroupId} created" . PHP_EOL);
-
-    // Duplicate Products
-    if ($sourceLangCode == 'en')
-        $productDAO = new \enterprise\daos\Product();
-    else
-        $productDAO = new \enterprise\daos\LangProduct($sourceLangCode);
-    $curProductId = ($fromProductId>0?$fromProductId:0);
-    do {
-        if ($sourceLangCode == 'en')
-            $products = enterprise_get_product_list($sourceSiteId, $sourceLangCode, $sourceGroupId, 1, 100, "`id`>{$curProductId}", '`id` ASC', '`tags`, `description`, `specifications`, `images`, `site_id`');
-        else
-            $products = enterprise_get_product_list($sourceSiteId, $sourceLangCode, $sourceGroupId, 1, 100, "elp.`product_id`>{$curProductId}", 'elp.`product_id` ASC', 'elp.`tags`, elp.`description`, elp.`specifications`, ep.`images`, elp.`site_id`');
-        if (!$products)
-            break;
-
-        if ($verbose >= 2)
-            fprintf(STDOUT, "Found " . count($products) . " product(s)" . PHP_EOL);
-
-        foreach ($products as $product) {
-            if ($verbose >= 3)
-                fprintf(STDOUT, "Duplicating product {$product['id']} ..." . PHP_EOL);
-
-            $product['specifications'] = ($product['specifications']?json_decode($product['specifications'], true):[]);
-            $product['images'] = ($product['images']?json_decode($product['images'], true):[]);
-
-            enterprise_admin_save_product($targetLangCode, 0, $product['brand_name'], $product['model_number'], $product['certification'], $product['place_of_origin'], $product['price'], $product['payment_terms'], $product['supply_ability'], $product['head_image_id'], $product['images'], $targetSiteId, $product['caption'], $product['description'], $targetGroupId, $product['min_order_quantity'], $product['delivery_time'], $product['packaging_details'], $product['specifications'], $product['tags']);
-
-            $curProductId = $product['id'];
-        }
-    } while(true);
+    $sourceGroupIdArray = explode(',', $sourceGroupIdList);
+    if (is_array($sourceGroupIdArray)) foreach ($sourceGroupIdArray as $id) {
+        duplicate_product_source_group($id, $sourceLangCode, $targetSiteId, 0, $targetLangCode, $fromProductId);
+    }
 
     if ($verbose >= 2)
-        fprintf(STDOUT, "Finish duplicate procedure for source group {$sourceGroupId}" . PHP_EOL);
+        fprintf(STDOUT, "Finish duplicate procedure" . PHP_EOL);
 }
 
 function main($argc, $argv)
