fa1d92a7668b18180d7babcd7099355bd2fa976d
full <2596244922@qq.com>
2018-09-20 08:47:24 +0800

    测试'

--- enterprise.h.php
+++ enterprise.h.php
@@ -70,6 +70,7 @@ define('ENTERPRISE_MAX_INDEX_PRODUCTS', 20);
 define('ENTERPRISE_PRODUCT_LIST_DEFAULT_PAGE_SIZE', 20);
 
 require_once __DIR__ . '/enterprise_product.h.php';
+require_once __DIR__ . '/Keyword.php';
 
 /** @var array Product Desc Mapping */
 $productDescMapping = array(// Key 'label' is deprecated.
