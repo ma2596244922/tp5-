2f3cbf48df0c81b61519380b6e143ddf3b20ab4c
full <2596244922@qq.com>
2018-09-20 09:58:07 +0800

    测试

--- enterprise.h.php
+++ enterprise.h.php
@@ -2551,8 +2551,12 @@ function enterprise_action_sets_product_list_proc($smarty, $site, $userAgent, $p
         $keywordDAO = new \enterprise\daos\Keyword();
         $condition= "`keyword`='".$phrase."'";
         $keyword = $keywordDAO->getOneBy($condition);
-        var_dump($keyword);
-        $smarty->assign('keyword',$keyword);
+        if($keyword.deleted=="0"){
+            $smarty->assign('keyword',$keyword);
+        }else{
+
+        }
+
     }
     $group = (($groupId && !is_array($groupId))?$smarty->getTemplateVars('group'):null);
 
